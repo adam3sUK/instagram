25a484000901cf055bfe596ddd3e879cb070dee1
Adam Harbisher <aharbisher@gmail.com>
2021-12-12 19:19:22 +0000

    Added sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_183722) do
+ActiveRecord::Schema.define(version: 2021_12_12_190225) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -42,7 +42,6 @@ ActiveRecord::Schema.define(version: 2021_12_12_183722) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "encrypted_password", limit: 128

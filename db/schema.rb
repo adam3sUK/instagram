6f0dec450f9d3ee5b38b342cac5a0179f83a91ea
Adam Harbisher <aharbisher@gmail.com>
2021-12-12 18:48:55 +0000

    Added clearance gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_182435) do
+ActiveRecord::Schema.define(version: 2021_12_12_183722) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -45,6 +45,11 @@ ActiveRecord::Schema.define(version: 2021_12_12_182435) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
   add_foreign_key "comments", "posts"

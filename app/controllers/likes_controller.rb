5d8e2c42119ff9781c62d09da870c6d58edb2205
Adam Harbisher <aharbisher@gmail.com>
2021-12-13 11:04:43 +0000

    Stopped more than one like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,25 @@
 class LikesController < ApplicationController
-  before_action :find_user
+  before_action :find_user, :find_post
 
   def create
-    @post = Post.find(params[:post_id])
-    @like = @post.likes.create(user_id: @user.id)
+    if already_liked?
+      flash[:notice] = "You've already liked this"
+    else
+      @post.likes.create(user_id: @user.id)
+    end
     redirect_to post_path(@post)
   end
     
   private
+
+  def already_liked?
+    Like.where(user_id: current_user.id, post_id:
+    params[:post_id]).exists?
+  end
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
 
   def find_user
     @user = User.find_by_id(current_user.id)
